2fd1dcfc14d41c1b786824b6d8f911caae081ab1
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-02-23 19:06:13 -0300

    Update hashids.php

--- src/Config/hashids.php
+++ src/Config/hashids.php
@@ -1,6 +1,30 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => env('HASHID_DEFAULT', 'falcon'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hashids Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the connections setup for your application. Example
+    | configuration has been included, but you may add as many connections as
+    | you would like.
+    |
+    */
+
     'connections' => [
         'falcon' => [
             'salt'     => env('HASHIDS_SALT', 'example'),
