0cfc787717318c7d62f053665fa85ebaa6060f73
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-04-22 20:28:26 -0300

    Update Database.php

--- src/Models/BackOffice/DataBase/Database.php
+++ src/Models/BackOffice/DataBase/Database.php
@@ -1,14 +1,17 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace FalconERP\Skeleton\Models\BackOffice\DataBase;
 
 use FalconERP\Skeleton\Models\BackOffice\DatabasesUsersAccess;
-use QuantumTecnology\ModelBasicsExtension\BaseModel;
 use FalconERP\Skeleton\Models\User;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use QuantumTecnology\ModelBasicsExtension\BaseModel;
 
 class Database extends BaseModel
 {
@@ -19,6 +22,19 @@ class Database extends BaseModel
     protected $fillable = [
         'base',
     ];
+
+    protected $casts = [
+        'base' => 'string',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the relationships that the model should have with
+    |
+    */
 
     /**
      * ActionsMovements function.
@@ -48,6 +64,15 @@ class Database extends BaseModel
         return $this->hasMany(DatabasesUsersAccess::class);
     }
 
+    /*
+    |--------------------------------------------------------------------------
+    | Scopes
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the scopes that the model should have with
+    |
+    */
+
     public function scopeByActiveAndUser(
         $query,
         bool $active,
@@ -58,10 +83,43 @@ class Database extends BaseModel
                 ->where('user_id', $user->id)
                 ->when($active, fn ($query) => $query->where('is_active', true)),
         ])
-        ->whereHas('databasesUsersAccess', function ($query) use ($active, $user) {
-            $query
-                ->where('user_id', $user->id)
-                ->when($active, fn ($query) => $query->where('is_active', true));
-        });
+            ->whereHas('databasesUsersAccess', function ($query) use ($active, $user) {
+                $query
+                    ->where('user_id', $user->id)
+                    ->when($active, fn ($query) => $query->where('is_active', true));
+            });
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Others
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the others that the model should have with
+    |
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Attributes
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the attributes that should be cast to native types.
+    |
+    */
+
+    protected function environment(): Attribute
+    {
+        return Attribute::get(fn () => $this->databasesUsersAccess()->first()?->environment);
+    }
+
+    protected function isActive(): Attribute
+    {
+        return Attribute::get(fn () => $this->databasesUsersAccess()->first()?->is_active);
+    }
+
+    protected function basePeopleId(): Attribute
+    {
+        return Attribute::get(fn () => $this->databasesUsersAccess()->first()?->base_people_id);
     }
 }
