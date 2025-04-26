1d006409cb0e9200503f9eb3170ed5e56d8e6a6c
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-04-25 22:04:47 -0300

    Update ShopEnum.php

--- src/Enums/Shop/ShopEnum.php
+++ src/Enums/Shop/ShopEnum.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace FalconERP\Skeleton\Enums\Shop;
 
 use QuantumTecnology\EnumBasicsExtension\BaseEnum;
@@ -9,8 +11,14 @@ abstract class ShopEnum extends BaseEnum
     public const STATUS_OPEN    = 'open';
     public const STATUS_CLOSEDS = 'closed';
 
-    public const TYPES_SERVICE   = 'service';
-    public const TYPES_MENU      = 'menu';
-    public const TYPES_ECCOMERCE = 'eccomerce';
-    public const TYPES_IFOOD     = 'ifood';
+    public const TYPES_SERVICE         = 'service';
+    public const TYPES_MENU            = 'menu';
+    public const TYPES_ECCOMERCE       = 'eccomerce';
+    public const TYPES_IFOOD           = 'ifood';
+    public const TYPES_SHOPPE          = 'shoppe';
+    public const TYPES_PDV             = 'pdv';
+    public const TYPES_SYSTEM          = 'system';
+    public const TYPES_MERCADO_LIVRE   = 'mercadolivre';
+    public const TYPES_RECEITA_FEDERAL = 'receitafederal';
+
 }
