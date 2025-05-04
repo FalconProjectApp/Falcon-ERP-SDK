03945443a9aadfb9ff297857a6c8e268d3b32693
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-05-04 17:52:11 -0300

    fix

--- src/Models/Erp/Setting.php
+++ src/Models/Erp/Setting.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace FalconERP\Skeleton\Models\Erp;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use QuantumTecnology\ModelBasicsExtension\BaseModel;
+use QuantumTecnology\ValidateTrait\Data;
 
 class Setting extends BaseModel
 {
@@ -14,4 +17,45 @@ class Setting extends BaseModel
         'value',
         'description',
     ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the relationships that the model should have with
+    |
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Scopes
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the scopes that the model should have with
+    |
+    */
+
+    public function scopeByName($query, $name): Data
+    {
+        return data(json_decode($query->where('name', $name)->first()->value));
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Others
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the others that the model should have with
+    |
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Attributes
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the attributes that should be cast to native types.
+    |
+    */
 }
--- src/Repositories/BigData/AuthRepository.php
+++ src/Repositories/BigData/AuthRepository.php
@@ -1,22 +1,25 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace FalconERP\Skeleton\Repositories\BigData;
 
+use FalconERP\Skeleton\Models\Erp\Setting;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
 use QuantumTecnology\ValidateTrait\Data;
 
 class AuthRepository
 {
+    public bool $success          = false;
+    public int $http_code         = 0;
+    public string $message        = 'not found';
+    public ?string $cnpj          = null;
+    public array | object $errors = [];
+    public array | object $data   = [];
     private string $urlApi;
-    private string $authorization;
-    private string $email;
-    private string $secret;
-    public bool $success        = false;
-    public int $http_code       = 0;
-    public string $message      = 'not found';
-    public ?string $cnpj        = null;
-    public array|object $errors = [];
-    public array|object $data   = [];
+    private string $email  = '';
+    private string $secret = '';
 
     public function __construct()
     {
@@ -25,18 +28,43 @@ class AuthRepository
             config('services.falcon.big_data.url_api')
         );
 
-        $this->email = config('services.falcon.big_data.email');
-        $this->secret = config('services.falcon.big_data.secret');
+        // $this->email  = config('services.falcon.big_data.email');
+        // $this->secret = config('services.falcon.big_data.secret');
     }
 
-    public function login(): self
+    public function login(?Data $data = null): self
     {
+        if (!($data instanceof Data)) {
+            $data = new Data();
+        }
+
+        if ($data->isEmpty() && class_exists(Setting::class)) {
+            $data = Setting::byName('datahub_access');
+        }
+
+        abort_if(
+            $data->isEmpty(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+            __('Errors: data is required'),
+        );
+
+        abort_if(
+            !$data->has('email'),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+            __('Errors: email is required'),
+        );
+        abort_if(
+            !$data->has('password'),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+            __('Errors: password is required'),
+        );
+
         $response = Http::retry(3, 2000, throw: false)
             ->acceptJson()
             ->asJson()
             ->post($this->urlApi, [
-                'email'  => $this->email,
-                'password' => $this->secret //TODO: mudar para secret
+                'email'    => $data->email,
+                'password' => $data->password, // TODO: mudar para secret
             ]);
 
         $this->http_code = $response->status();
