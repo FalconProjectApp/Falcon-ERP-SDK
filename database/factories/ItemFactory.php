72d39b292ed74a3cf7ce969257240b1b31907408
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-06-17 23:08:14 -0300

    fix

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace Database\Factories\FalconERP\Skeleton;
+namespace FalconERP\Skeleton\Database\Factories;
 
 use FalconERP\Skeleton\Models\Erp\Stock\Item;
 use Illuminate\Database\Eloquent\Factories\Factory;
--- database/factories/ProductCommentFactory.php
+++ database/factories/ProductCommentFactory.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace Database\Factories\FalconERP\Skeleton;
+namespace FalconERP\Skeleton\Database\Factories;
 
 use FalconERP\Skeleton\Models\Erp\Stock\ProductComment;
 use Illuminate\Database\Eloquent\Factories\Factory;
