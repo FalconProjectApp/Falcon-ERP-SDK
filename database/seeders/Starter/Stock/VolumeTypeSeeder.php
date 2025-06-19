7a0f40fcfbba066cbf01a83a33e503a10fbe56fb
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-06-19 16:12:02 -0300

    Update VolumeTypeSeeder.php

--- database/seeders/Starter/Stock/VolumeTypeSeeder.php
+++ database/seeders/Starter/Stock/VolumeTypeSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Database\Seeders\Starter\Stock;
 
@@ -10,38 +10,30 @@ use Illuminate\Database\Seeder;
 class VolumeTypeSeeder extends Seeder
 {
     protected $items = [
-        [
-            'id'          => 0,
-            'description' => 'Caixa com 12',
-            'initials'    => 'CX12',
-            'created_at'  => now(),
-            'updated_at'  => now(),
-        ],
-        [
-            'id'          => 1,
-            'description' => 'Caixa com 6',
-            'initials'    => 'CX6',
-            'created_at'  => now(),
-            'updated_at'  => now(),
-        ],
-        [
-            'id'          => 2,
-            'description' => 'Unidade',
-            'initials'    => 'UN',
-            'created_at'  => now(),
-            'updated_at'  => now(),
-        ],
-        [
-            'id'          => 3,
-            'description' => 'Litro',
-            'initials'    => 'LT',
-            'created_at'  => now(),
-            'updated_at'  => now(),
-        ],
+        ['description' => 'Caixa com 12', 'initials' => 'CX12'],
+        ['description' => 'Caixa com 6', 'initials' => 'CX6'],
+        ['description' => 'Unidade', 'initials' => 'UN'],
+        ['description' => 'Litro', 'initials' => 'LT'],
     ];
 
     public function run(): void
     {
-        VolumeType::insert($this->items);
+        collect($this->items)->each(function ($releaseType) {
+            $releaseTypeExists = VolumeType::query()
+                ->where('description', $releaseType[0])
+                ->where('initials', $releaseType[1])
+                ->exists();
+
+            if ($releaseTypeExists) {
+                return;
+            }
+
+            VolumeType::query()->insert([
+                'description' => $releaseType[0],
+                'initials'    => $releaseType[1],
+                'created_at'  => now(),
+                'updated_at'  => now(),
+            ]);
+        });
     }
 }
