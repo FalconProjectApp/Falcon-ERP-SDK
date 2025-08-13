c7f075ce83eac8cd6f4ccb7fbaff6d1bd13867a3
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-08-13 20:09:08 -0300

    Add new product attribute constants to trait
    Added constants for volume type ID, created/updated timestamps, and various balance and value attributes to ProductCollunsTrait. Also renamed ATTRIBUTE_EAN to V_ATTRIBUTE_EAN for consistency.

--- src/Models/Erp/Stock/Traits/Request/ProductCollunsTrait.php
+++ src/Models/Erp/Stock/Traits/Request/ProductCollunsTrait.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace FalconERP\Skeleton\Models\Erp\Stock\Traits\Request;
 
@@ -8,11 +8,19 @@ trait ProductCollunsTrait
 {
     public const ATTRIBUTE_ID              = 'id';
     public const ATTRIBUTE_GROUPS_ID       = 'group_id';
+    public const ATTRIBUTE_VOLUME_TYPE_ID  = 'volume_types_id';
     public const ATTRIBUTE_STATUS          = 'status';
     public const ATTRIBUTE_DESCRIPTION     = 'description';
-    public const ATTRIBUTE_EAN             = 'ean';
+    public const V_ATTRIBUTE_EAN           = 'ean';
     public const ATTRIBUTE_LAST_BUY_VALUE  = 'last_buy_value';
     public const ATTRIBUTE_LAST_SELL_VALUE = 'last_sell_value';
     public const ATTRIBUTE_LAST_RENT_VALUE = 'last_rent_value';
     public const ATTRIBUTE_OBSERVATIONS    = 'observations';
+    public const ATTRIBUTE_CREATED_AT      = 'created_at';
+    public const ATTRIBUTE_UPDATED_AT      = 'updated_at';
+
+    public const V_ATTRIBUTE_BALANCE_STOCK_TOTAL   = 'balance_stock_total';
+    public const V_ATTRIBUTE_BALANCE_TRANSIT_TOTAL = 'balance_transit_total';
+    public const V_ATTRIBUTE_BALANCE_TOTAL         = 'balance_total';
+    public const V_ATTRIBUTE_VALUE_TOTAL           = 'value_total';
 }
