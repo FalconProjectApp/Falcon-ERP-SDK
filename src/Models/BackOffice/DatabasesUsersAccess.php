76c4da003919eebc1b7a94d3af8fcb2e0bcf0b83
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-09-01 21:43:12 -0300

    Add is_active field to DatabasesUsersAccess model
    Introduces the is_active attribute to the DatabasesUsersAccess model, including fillable and cast definitions. Also adds strict types declaration and minor formatting improvements.

--- src/Models/BackOffice/DatabasesUsersAccess.php
+++ src/Models/BackOffice/DatabasesUsersAccess.php
@@ -1,24 +1,27 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace FalconERP\Skeleton\Models\BackOffice;
 
 use FalconERP\Skeleton\Models\BackOffice\DataBase\Database;
-use QuantumTecnology\ModelBasicsExtension\BaseModel;
 use FalconERP\Skeleton\Models\Erp\People\People;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Config;
+use QuantumTecnology\ModelBasicsExtension\BaseModel;
 
 class DatabasesUsersAccess extends BaseModel
 {
     use HasFactory;
+    public $timestamps = false;
 
     protected $connection = 'pgsql';
-    public $timestamps    = false;
 
     protected $fillable = [
         'database_id',
         'base_people_id',
+        'is_active',
         'created_at',
         'updated_at',
     ];
@@ -26,6 +29,7 @@ class DatabasesUsersAccess extends BaseModel
     protected $casts = [
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
+        'is_active'  => 'boolean',
     ];
 
     public function database(): BelongsTo
@@ -40,7 +44,7 @@ class DatabasesUsersAccess extends BaseModel
     {
         Config::set(
             'database.connections.tenant.database',
-            'bc_'.request()->database->base
+            'bc_' . request()->database->base
         );
 
         $this->setConnection('tenant');
