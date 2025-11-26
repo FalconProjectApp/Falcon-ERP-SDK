e799fafdfcce5943a895b641b58c74abb79fdbc3
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-11-26 00:29:10 -0300

    Add beta environment configs to falconservices.php
    Introduced 'beta' environment settings for all Falcon service URLs in the configuration file, enabling easier testing and deployment to a dedicated beta environment.

--- src/Config/falconservices.php
+++ src/Config/falconservices.php
@@ -18,6 +18,9 @@ return [
         'production' => [
             'url_api' => env('FALCON_AUTH_URL', 'https://nve3lgblkk.execute-api.us-east-1.amazonaws.com'),
         ],
+        'beta' => [
+            'url_api' => env('FALCON_AUTH_URL', 'http://192.168.100.144:8000/auth2'),
+        ],
         'local' => [
             'url_api' => env('FALCON_AUTH_URL', 'http://localhost:8000'),
         ],
@@ -25,6 +28,9 @@ return [
     'big_data' => [
         'production' => [
             'url_api' => env('FALCON_BIGDATA_URL', 'https://dmy48yjy7i.execute-api.us-east-1.amazonaws.com'),
+        ],
+        'beta' => [
+            'url_api' => env('FALCON_BIGDATA_URL', 'http://192.168.100.144:8000/data-hub'),
         ],
         'local' => [
             'url_api' => env('FALCON_BIGDATA_URL', 'http://localhost:8007'),
@@ -34,6 +40,9 @@ return [
         'production' => [
             'url_api' => env('FALCON_FINANCE_URL', 'https://5g2i2wdfx5.execute-api.us-east-1.amazonaws.com'),
         ],
+        'beta' => [
+            'url_api' => env('FALCON_FINANCE_URL', 'http://192.168.100.144:8000/finance'),
+        ],
         'local' => [
             'url_api' => env('FALCON_FINANCE_URL', 'http://localhost:8003'),
         ],
@@ -41,6 +50,9 @@ return [
     'stock' => [
         'production' => [
             'url_api' => env('FALCON_STOCK_URL', 'https://4e84tfx5bb.execute-api.us-east-1.amazonaws.com'),
+        ],
+        'beta' => [
+            'url_api' => env('FALCON_STOCK_URL', 'http://192.168.100.144:8000/stock'),
         ],
         'local' => [
             'url_api' => env('FALCON_STOCK_URL', 'http://localhost:8004'),
@@ -50,6 +62,9 @@ return [
         'production' => [
             'url_api' => env('FALCON_FISCAL_URL', 'https://edupw3uheh.execute-api.us-east-1.amazonaws.com'),
         ],
+        'beta' => [
+            'url_api' => env('FALCON_FISCAL_URL', 'http://192.168.100.144:8000/fiscal'),
+        ],
         'local' => [
             'url_api' => env('FALCON_FISCAL_URL', 'http://localhost:8002'),
         ],
@@ -57,6 +72,9 @@ return [
     'shop' => [
         'production' => [
             'url_api' => env('FALCON_SHOP_URL', 'https://9f7hkz4y84.execute-api.us-east-1.amazonaws.com'),
+        ],
+        'beta' => [
+            'url_api' => env('FALCON_SHOP_URL', 'http://192.168.100.144:8000/shop'),
         ],
         'local' => [
             'url_api' => env('FALCON_SHOP_URL', 'http://localhost:8008'),
